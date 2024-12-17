7eec00f0b7f9691ef3b58fdc527af88b5ba37a44
Scott Vorthmann <scott@vorthmann.org>
2024-12-16 21:20:51 -0800

    Fixed double drop-downs

--- dropdowns.js
+++ dropdowns.js
@@ -6,12 +6,14 @@ const submenuFractals = document .getElementById( "submenu-fractals" );
 
 document .getElementById( "toggle-holography" ) .addEventListener( "click", evt => {
   if ( submenuHolography .style .display === "none" ) {
+    submenuFractals .style .display = "none";
     submenuHolography .style .display = "block";
     evt .stopPropagation(); // don't let the event fall through to the nav listener below
   }
 });
 document .getElementById( "toggle-fractals" ) .addEventListener( "click", evt => {
   if ( submenuFractals .style .display === "none" ) {
+    submenuHolography .style .display = "none";
     submenuFractals .style .display = "block";
     evt .stopPropagation(); // don't let the event fall through to the nav listener below
   }
